dc822c83c41f52340e6c45afcb3c2309321da750
Radoslav Radkov <radoslav@Radoslavs-MacBook-Pro.local>
2024-11-13 08:10:25 +0200

    new tutorial code was added based on Derek Banas course. Part 1

--- happyPersons.pl
+++ happyPersons.pl
@@ -9,3 +9,13 @@ runs(albert) :- happy(albert).
 dances(alice) :-
     happy(alice),
     with_albert(alice).
+
+does_alice_dance :-
+    dances(alice),
+    write('When Alice is happy and with Albert she dances').
+
+near_water(bob).
+
+swims(bob) :-
+    happy(bob),
+    near_water(bob).
---
+++ parents.pl
@@ -0,0 +1,18 @@
+male(charlie).
+male(dan).
+male(edward).
+
+female(alice).
+female(betsy).
+female(diana).
+
+parent(albert, bob).
+parent(albert, bill).
+parent(albert, betsy).
+
+parent(alice, bob).
+parent(alice, bill).
+parent(alice, betsy).
+
+parent(bob, carl).
+parent(bob, charlie).
